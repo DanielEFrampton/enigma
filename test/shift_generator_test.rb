1dc6c47b199bc1c2d16b753b34b644590a8f438a
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 11:37:49 -0600

    Add test for .get_last_four_of method

--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -17,7 +17,12 @@ class ShiftGeneratorTest < Minitest::Test
     assert_equal 5, @shift_generator.add(2, "3")
   end
 
-  def test_it_can_square_a_number
+  def test_it_can_square_a_number_as_ints_or_strings
     assert_equal 12148227961, @shift_generator.square(110219)
+    assert_equal 12148227961, @shift_generator.square("110219")
+  end
+
+  def test_it_can_get_last_four_digits_of_given_integer
+    assert_equal 7961, @shift_generator.get_last_four_of(12148227961)
   end
 end
