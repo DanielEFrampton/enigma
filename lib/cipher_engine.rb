c8cfa2948c66f5d092aded27a66dd10a3c6a0861
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 12:39:14 -0600

    Add class variable to CipherEngine holding charset and getter method

--- lib/cipher_engine.rb
+++ lib/cipher_engine.rb
@@ -1,2 +1,15 @@
+require './lib/shift_generator'
+
 class CipherEngine
+  @@charset = ("a".."z").to_a << " "
+
+  def self.charset
+    @@charset
+  end
+
+  def self.encrypt
+  end
+
+  def self.decrypt
+  end
 end
