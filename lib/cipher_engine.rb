0185a4cf367f1d220a03b06432ead4615ab5d120
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 14:02:44 -0600

    Add test-passing .encrypt method

--- lib/cipher_engine.rb
+++ lib/cipher_engine.rb
@@ -7,9 +7,20 @@ class CipherEngine
     @@charset
   end
 
-  def self.encrypt
+  def self.encrypt(message, key, date)
+    shift_values = ShiftGenerator.generate_shift_values(key, date)
+    encrypted_chars = message.chars.collect_concat do |char|
+      if charset.include?(char)
+        encrypted_char = charset[(charset.index(char) + shift_values.first) % 27]
+        shift_values.rotate!(1)
+      else
+        encrypted_char = char
+      end
+      encrypted_char
+    end
+    encrypted_chars.join
   end
 
-  def self.decrypt
+  def self.decrypt(message, key, date)
   end
 end
