4e83a8a755e5310e5ad90790c8b8261fb113b53e
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 12:21:24 -0600

    Refactor all ShiftGenerator methods as class methods and update tests

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -1,30 +1,30 @@
 class ShiftGenerator
 
-  def add(addend_1, addend_2)
+  def self.add(addend_1, addend_2)
     addend_1.to_i + addend_2.to_i
   end
 
-  def square(number)
+  def self.square(number)
     number.to_i * number.to_i
   end
 
-  def get_last_four_of(number)
+  def self.get_last_four_of(number)
     number.to_s.slice(-4, 4)
   end
 
-  def split_into_keys(number)
+  def self.split_into_keys(number)
     number.chars.each_cons(2).map { |pair| pair.join.to_i }
   end
 
-  def generate_offsets(date)
+  def self.generate_offsets(date)
     get_last_four_of(square(date))
   end
 
-  def add_offsets_to_keys(offsets, keys)
+  def self.add_offsets_to_keys(offsets, keys)
     offsets.chars.map.with_index { |char, ind| char.to_i + keys[ind] }
   end
 
-  def generate_shift_values(key, date)
+  def self.generate_shift_values(key, date)
     add_offsets_to_keys(generate_offsets(date), split_into_keys(key))
   end
 end
--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -12,36 +12,36 @@ class ShiftGeneratorTest < Minitest::Test
   end
 
   def test_it_can_add_two_numbers_as_ints_or_strings
-    assert_equal 5, @shift_generator.add(2, 3)
-    assert_equal 5, @shift_generator.add("2", "3")
-    assert_equal 5, @shift_generator.add(2, "3")
+    assert_equal 5, ShiftGenerator.add(2, 3)
+    assert_equal 5, ShiftGenerator.add("2", "3")
+    assert_equal 5, ShiftGenerator.add(2, "3")
   end
 
   def test_it_can_square_a_number_as_ints_or_strings
-    assert_equal 12148227961, @shift_generator.square(110219)
-    assert_equal 12148227961, @shift_generator.square("110219")
+    assert_equal 12148227961, ShiftGenerator.square(110219)
+    assert_equal 12148227961, ShiftGenerator.square("110219")
   end
 
   def test_it_can_get_last_four_digits_of_given_integer
-    assert_equal "7961", @shift_generator.get_last_four_of(12148227961)
-    assert_equal "7961", @shift_generator.get_last_four_of("12148227961")
+    assert_equal "7961", ShiftGenerator.get_last_four_of(12148227961)
+    assert_equal "7961", ShiftGenerator.get_last_four_of("12148227961")
   end
 
   def test_it_can_split_five_digit_string_into_four_two_digit_number_keys
-    assert_equal [12, 23, 34, 45], @shift_generator.split_into_keys("12345")
-    assert_equal [0, 1, 10, 0], @shift_generator.split_into_keys("00100")
+    assert_equal [12, 23, 34, 45], ShiftGenerator.split_into_keys("12345")
+    assert_equal [0, 1, 10, 0], ShiftGenerator.split_into_keys("00100")
   end
 
   def test_it_can_add_offsets_to_keys
-    assert_equal [19, 32, 40, 46], @shift_generator.add_offsets_to_keys("7961", [12, 23, 34, 45])
-    assert_equal [0, 2, 10, 1], @shift_generator.add_offsets_to_keys("0101", [0, 1, 10, 0])
+    assert_equal [19, 32, 40, 46], ShiftGenerator.add_offsets_to_keys("7961", [12, 23, 34, 45])
+    assert_equal [0, 2, 10, 1], ShiftGenerator.add_offsets_to_keys("0101", [0, 1, 10, 0])
   end
 
   def test_it_can_generate_offsets_from_date
-    assert_equal "7961", @shift_generator.generate_offsets("110219")
+    assert_equal "7961", ShiftGenerator.generate_offsets("110219")
   end
 
   def test_it_can_generate_shift_values_from_key_number_and_date
-    assert_equal [19, 32, 40, 46], @shift_generator.generate_shift_values("12345", "110219")
+    assert_equal [19, 32, 40, 46], ShiftGenerator.generate_shift_values("12345", "110219")
   end
 end
