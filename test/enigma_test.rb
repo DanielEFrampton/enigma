ea6b1b499bc2aa2c5a60442d107b2de05e533849
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 15:00:59 -0600

    Write initial test for Enigma class

---
+++ test/enigma_test.rb
@@ -0,0 +1,14 @@
+require './test/test_helper'
+require './lib/cipher_engine'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+end
