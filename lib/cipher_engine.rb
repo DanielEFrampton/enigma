faf341efe39b2670c499e9506eae36b9c734b35c
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 14:06:09 -0600

    Write passing .decrypt method

--- lib/cipher_engine.rb
+++ lib/cipher_engine.rb
@@ -22,5 +22,16 @@ class CipherEngine
   end
 
   def self.decrypt(message, key, date)
+    shift_values = ShiftGenerator.generate_shift_values(key, date)
+    encrypted_chars = message.chars.collect_concat do |char|
+      if charset.include?(char)
+        encrypted_char = charset[(charset.index(char) - shift_values.first) % 27]
+        shift_values.rotate!(1)
+      else
+        encrypted_char = char
+      end
+      encrypted_char
+    end
+    encrypted_chars.join
   end
 end
