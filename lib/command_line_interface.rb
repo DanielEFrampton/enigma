c81b1edbac20e15858c921cdd2a30696f7772310
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 20:45:47 -0600

    Write test-passing initialize method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,10 +1,16 @@
+require './lib/enigma'
+
 class CommandLineInterface
+  attr_reader :enigma
+
+  def initialize
+    @enigma = Enigma.new
+  end
 
   def parse_argv(argv)
-    {  input_path: argv[0],
-       output_path: argv[1],
-       key: argv[2],
-       date: argv[3]
-    }
+      {
+        input_path: argv[0],
+        output_path: argv[1]
+      }
   end
 end
