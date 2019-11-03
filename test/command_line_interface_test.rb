37ee6578d54b04816cbcedb99c4eb6694839c3ee
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 20:45:33 -0600

    Add test for initializing with instance of Enigma class

--- test/command_line_interface_test.rb
+++ test/command_line_interface_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/command_line_interface'
+require './lib/enigma'
 
 class CommandLineInterfaceTest < Minitest::Test
   def setup
@@ -10,12 +11,14 @@ class CommandLineInterfaceTest < Minitest::Test
     assert_instance_of CommandLineInterface, @command_line
   end
 
+  def test_it_initializes_with_enigma_instance
+    assert_instance_of Enigma, @command_line.enigma
+  end
+
   def test_it_can_parse_argv
     placeholder_input = ["message.txt", "encrypted.txt", "82648", "240818"]
     expected = { input_path: "message.txt",
-                 output_path: "encrypted.txt",
-                 key: "82648",
-                 date: "240818" }
+                 output_path: "encrypted.txt" }
     assert_equal expected, @command_line.parse_argv(placeholder_input)
   end
 end
