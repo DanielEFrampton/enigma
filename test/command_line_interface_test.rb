ad8278e22fdb4e2254056738cb9b857db7066c64
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 19:18:24 -0600

    Write test for .parse_argv method

--- test/command_line_interface_test.rb
+++ test/command_line_interface_test.rb
@@ -9,4 +9,13 @@ class CommandLineInterfaceTest < Minitest::Test
   def test_it_exists
     assert_instance_of CommandLineInterface, @command_line
   end
+
+  def test_it_can_parse_argv
+    placeholder_input = ["message.txt", "encrypted.txt", "82648", "240818"]
+    expected = { input_path: "message.txt",
+                 output_path: "encrypted.txt",
+                 key: "82648",
+                 date: "240818" }
+    assert_equal expected, @command_line_interface.parse_argv(placeholder_input)
+  end
 end
