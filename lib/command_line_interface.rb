4c7591a80250d66f5d51635d5d54a4d6140c7dab
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 21:31:23 -0600

    Write test-passing .print_terminal_report method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -18,4 +18,8 @@ class CommandLineInterface
   def write_string_to_file(output_path, string)
     File.write(output_path, string)
   end
+
+  def print_terminal_report
+    puts "Created '#{output_path}' with the key #{key} and date #{date}"
+  end
 end
