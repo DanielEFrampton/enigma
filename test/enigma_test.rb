2ad0e6378052db7a301e4e9df6025b679734cf61
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 21:22:11 -0600

    Refactor Enigma encryption and decryption tests to change output to hash

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -27,15 +27,25 @@ class EnigmaTest < Minitest::Test
     @enigma.stubs(:generate_key).returns("02715")
     @enigma.stubs(:get_todays_date).returns("040895")
     CipherEngine.stubs(:encrypt).with("hello world!", "02715", "040895").returns("keder ohulw!")
-    assert_equal "keder ohulw!", @enigma.encrypt("hello world!", "02715", "040895")
-    assert_equal "keder ohulw!", @enigma.encrypt("hello world!", "02715")
-    assert_equal "keder ohulw!", @enigma.encrypt("hello world!")
+    expected = {
+                  encryption: "keder ohulw!",
+                  key: "02715",
+                  date: "040895"
+               }
+    assert_equal expected, @enigma.encrypt("hello world!", "02715", "040895")
+    assert_equal expected, @enigma.encrypt("hello world!", "02715")
+    assert_equal expected, @enigma.encrypt("hello world!")
   end
 
   def test_it_can_decrypt_given_message_with_or_without_date
     @enigma.stubs(:get_todays_date).returns("040895")
     CipherEngine.stubs(:decrypt).with("keder ohulw!", "02715", "040895").returns("hello world!")
-    assert_equal "hello world!", @enigma.decrypt("keder ohulw!", "02715", "040895")
-    assert_equal "hello world!", @enigma.decrypt("keder ohulw!", "02715")
+    expected = {
+                  encryption: "hello world!",
+                  key: "02715",
+                  date: "040895"
+               }
+    assert_equal expected, @enigma.decrypt("keder ohulw!", "02715", "040895")
+    assert_equal expected, @enigma.decrypt("keder ohulw!", "02715")
   end
 end
