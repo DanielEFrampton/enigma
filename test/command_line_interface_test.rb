2f917875efe512c980fbf2a622fdab3c57103a57
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 21:01:39 -0600

    Refactor CLI test to move ARGV parsing into initialization

--- test/command_line_interface_test.rb
+++ test/command_line_interface_test.rb
@@ -4,7 +4,8 @@ require './lib/enigma'
 
 class CommandLineInterfaceTest < Minitest::Test
   def setup
-    @command_line = CommandLineInterface.new
+    @placeholder_input = ["message.txt", "encrypted.txt", "82648", "240818"]
+    @command_line = CommandLineInterface.new(placeholder_input)
   end
 
   def test_it_exists
@@ -15,10 +16,10 @@ class CommandLineInterfaceTest < Minitest::Test
     assert_instance_of Enigma, @command_line.enigma
   end
 
-  def test_it_can_parse_argv
-    placeholder_input = ["message.txt", "encrypted.txt", "82648", "240818"]
-    expected = { input_path: "message.txt",
-                 output_path: "encrypted.txt" }
-    assert_equal expected, @command_line.parse_argv(placeholder_input)
+  def test_it_initializes_with_attributes_from_argv
+    assert_equal @placeholder_input[0], @command_line.input_path
+    assert_equal @placeholder_input[1], @command_line.output_path
+    assert_equal @placeholder_input[2], @command_line.key
+    assert_equal @placeholder_input[3], @command_line.date
   end
 end
