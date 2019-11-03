7fcc1a2efde906f344a1b66533accfd12c03ca39
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-02 21:43:21 -0600

    Refactor test and class to make all methods class methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,15 +3,15 @@ require 'date'
 
 class Enigma
 
-  def get_todays_date
+  def self.get_todays_date
     Date.new.strftime("%m%d%y")
   end
 
-  def generate_key
+  def self.generate_key
     rand(99999).to_s.rjust(5, "0")
   end
 
-  def encrypt(message, key = generate_key, date = get_todays_date)
+  def self.encrypt(message, key = generate_key, date = get_todays_date)
     encrypted_message = CipherEngine.encrypt(message, key, date)
     {
       encryption: encrypted_message,
@@ -20,7 +20,7 @@ class Enigma
     }
   end
 
-  def decrypt(message, key, date = get_todays_date)
+  def self.decrypt(message, key, date = get_todays_date)
     decrypted_message = CipherEngine.decrypt(message, key, date)
     {
       decryption: decrypted_message,
