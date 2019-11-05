31808c26de1d294504c67f15e275d698a73da4db
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-05 11:07:55 -0700

    Write test for .get_key_permutations method

--- test/cipher_engine_test.rb
+++ test/cipher_engine_test.rb
@@ -57,6 +57,11 @@ class CipherEngineTest < Minitest::Test
     assert_equal [8, 2, 3, 4], CipherEngine.crack_keys([14, 5, 5, 8], "291018")
   end
 
+  def test_it_makes_arrays_of_key_permutation_strings
+    expected = [%w(08 35 62 89), %w(02 29 56 83), %w(03 30 57 84), %w(04 31 58 85)]
+    assert_equal expected, CipherEngine.get_key_permutations([8, 2, 3, 4])
+  end
+
   def test_it_can_crack_key_from_encrypted_message_and_date
     skip
     assert_equal "08304", CipherEngine.crack_key("vjqtbeaweqihssi", "291018")
