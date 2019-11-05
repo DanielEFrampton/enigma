5e8bc013e3113ed2028dc37d91f4c933ae192bb2
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-04 20:32:32 -0700

    Refactor methods to simplify encrypt or decrypt switch

--- lib/cipher_engine.rb
+++ lib/cipher_engine.rb
@@ -7,33 +7,29 @@ class CipherEngine
     @@charset
   end
 
-  def self.shift_character(char, shift, type)
-    if type == :encrypt
-      charset[(get_index(char) + shift) % 27]
-    elsif type == :decrypt
-      charset[(get_index(char) - shift) % 27]
-    end
+  def self.shift_character(char, shift, operation)
+    charset[get_index(char).send(operation, shift) % 27]
   end
 
   def self.get_index(char)
     charset.index(char)
   end
 
-  def self.engine(message, key, date, type)
+  def self.engine(message, key, date, operation)
     shift_values = ShiftGenerator.generate_shift_values(key, date)
     message.chars.reduce("") do |new_string, char|
       next new_string + char if !charset.include?(char)
       shift_values.rotate!(1) unless new_string.empty?
-      new_string.concat(shift_character(char, shift_values.first, type))
+      new_string.concat(shift_character(char, shift_values.first, operation))
     end
   end
 
   def self.encrypt(message, key, date)
-    engine(message, key, date, :encrypt)
+    engine(message, key, date, :+)
   end
 
   def self.decrypt(message, key, date)
-    engine(message, key, date, :decrypt)
+    engine(message, key, date, :-)
   end
 
   def self.shift_between(decrypt_char, encrypt_char)
