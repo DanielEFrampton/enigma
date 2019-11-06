f8fb6b27d6cb0fdb8913787732d632137dcfeeaf
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-06 00:13:21 -0700

    Refactor find_original_keys method to reduce length and complexity

--- lib/cipher_engine.rb
+++ lib/cipher_engine.rb
@@ -56,44 +56,20 @@ class CipherEngine
 
   def self.get_key_permutations(keys)
     keys.map do |key|
-      [
-        (key.to_s.length == 1 ? key.to_s.prepend("0") : key.to_s),
+      [ (key.to_s.length == 1 ? key.to_s.prepend("0") : key.to_s),
         (key + 27).to_s, (key + 54).to_s, (key + 81).to_s
       ].reject { |key_perm| key_perm.length > 2 }
     end
   end
 
   def self.find_original_keys(perms)
-    originals = []
     perms[0].each do |perm_1|
-      # Does a matching permutation exist on 2nd array? If not, next.
-      unless perms[1].any? { |perm| perm[0] == perm_1[-1] }
-        next
-      else
-        # If so, save found permutation as 2nd.
-        perm_2 = perms[1].find { |perm| perm[0] == perm_1[-1] }
-        # Does a matching permutation exist on 3rd array? If not, next.
-        unless perms[2].any? { |perm| perm[0] == perm_2[-1] }
-          next
-        else
-          # If so, save found permutation as 3rd.
-          perm_3 = perms[2].find { |perm| perm[0] == perm_2[-1] }
-          # Does a matching permutation exist on 4th array? If not, next.
-          unless perms[3].any? { |perm| perm[0] == perm_3[-1] }
-            next
-          else
-            # If so, save as 4th to results array and begin backing out.
-            originals[3] = perms[3].find { |perm| perm[0] == perm_3[-1] }
-          end
-        # Save third to results array, back out.
-        originals[2] = perm_3
-        end
-      # Save second to results array, back out.
-      originals[1] = perm_2
-      end
-      originals[0] = perm_1
+      keys = [perm_1]
+      keys[1] = perms[1].find { |perm| perm[0] == keys[0][-1] } || next
+      keys[2] = perms[2].find { |perm| perm[0] == keys[1][-1] } || next
+      keys[3] = perms[3].find { |perm| perm[0] == keys[2][-1] } || next
+      return keys
     end
-    originals
   end
 
   def self.combine_keys(keys)
